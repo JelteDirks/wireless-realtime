76937cef9c3f3fb240ec39744da038e50e73e542
Jelte Dirks <j.dirks@outlook.com>
2025-07-01 23:15:26 +0200

    bare minimum to get working qemu vga text output
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Real-Time Operating System in Rust
+
+- set rust toolchain to nightly build `rustup override set nightly`
+- install the rust source `rustup component add rust-src`
+- install QEMU `brew install qemu`
+- install llvm preview tools `rustup component add llvm-tools-preview`
+- install bootimage using `cargo install bootimage`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+const VGA_BUFFER_ADDR:u32 = 0xb8000;
+const LIGHT_CYAN: u8 =  0xb;
+
+static HELLOWORLD: &[u8] = b"DEEZ NUTS";
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    // casts to pointer of u8 to write into the buffer one u8 at a time
+    let vga_buffer_start = VGA_BUFFER_ADDR as *mut u8;
+
+    for (i, byte) in HELLOWORLD.iter().enumerate() {
+        unsafe {
+            *vga_buffer_start.offset(i as isize * 2) = *byte;
+            *vga_buffer_start.offset(i as isize * 2 + 1) = LIGHT_CYAN;
+        }
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn handle_panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
